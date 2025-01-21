26b39f60d39ec40af4870b7e6a1f6d4120e9ffa4
Nathan Donato <njdonato30@gmail.com>
2025-01-20 16:10:44 -0800

    latest commit with analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import Home from "./pages/Home";
+import { useEffect } from "react";
+import { initGA, logPageView } from "./analytics";
 
 function App() {
+  useEffect(() => {
+    initGA();
+    logPageView();
+  }, []);
+
   return <Home />;
 }
 
